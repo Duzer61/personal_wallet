cdf89368d3860ba55d69a3db8464547893b36318
Данил Кочетов <dmi194@yandex.ru>
2024-05-27 11:52:11 +0300

    Убрал инфо-сообщения от методов во время тестирования

--- test_runner.py
+++ test_runner.py
@@ -1,10 +1,15 @@
+import io
+import sys
 import unittest
 
 
 def run_tests():
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('tests')
     unittest.TextTestRunner().run(test_suite)
+    sys.stdout = original_stdout
 
 
 if __name__ == '__main__':
